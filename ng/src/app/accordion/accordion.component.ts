96b982ec74ab7cec6d34eaa6af28d535dc6f8374
yannis <yannis@work>
2018-10-02 16:05:52 +0300

    Added ngx-bootstrap

---
+++ ng/src/app/accordion/accordion.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'accordion-basic',
+  templateUrl: './accordion.html'
+})
+export class AccordionBasicComponent {
+}
+
--- ng/src/app/app.module.ts
+++ ng/src/app/app.module.ts
@@ -3,20 +3,25 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+
 import { AppComponent } from './app.component';
 import { InsertAuthorComponent } from './author/insert-author/insert-author.component';
 import { ListAuthorsComponent } from './author/list-authors/list-authors.component';
+import { AccordionBasicComponent } from './accordion/accordion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InsertAuthorComponent,
-    ListAuthorsComponent
+    ListAuthorsComponent,
+    AccordionBasicComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AccordionModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
