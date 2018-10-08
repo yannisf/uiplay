4ad997e68bd63dd5f8d16401595132a1ac7adc96
yannis <yannis@work>
2018-10-08 16:56:28 +0300

    Restructuring REST API tests

--- src/test/java/fraglab/library/it/container/AuthorRestApiTest.java
+++ src/test/java/fraglab/library/it/container/AuthorRestApiTest.java
@@ -1,5 +1,6 @@
 package fraglab.library.it.container;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fraglab.library.Author;
 import fraglab.library.Book;
@@ -7,7 +8,8 @@ import fraglab.library.it.container.embedded.EmbeddedServer;
 import fraglab.library.valueobject.AuthorValue;
 import fraglab.library.valueobject.BookValue;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -16,6 +18,8 @@ import java.util.List;
 import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpMethod.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 public class AuthorRestApiTest extends AbstractRestIntegrationTest {
 
@@ -26,65 +30,91 @@ public class AuthorRestApiTest extends AbstractRestIntegrationTest {
 
     @Test
     public void testPost() throws IOException {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
         String authorName = getRandomString();
-        AuthorValue authorValue = new AuthorValue(authorName);
-        String jsonAuthor = MAPPER.writeValueAsString(authorValue);
-        HttpEntity<String> entity = new HttpEntity<>(jsonAuthor, headers);
-        ResponseEntity<AuthorValue> exchange = restTemplate.exchange(AUTHOR_URL, HttpMethod.POST, entity, AuthorValue.class);
-
-        AuthorValue postAuthor = exchange.getBody();
-        Long id = Objects.requireNonNull(postAuthor).getId();
-        Author getAuthor = restTemplate.getForObject(String.format("%s/%s", AUTHOR_URL, id), Author.class);
-        assertThat(Objects.requireNonNull(getAuthor).getName()).isEqualTo(authorName);
-
+        HttpEntity<String> authorEntity = createAuthorEntityFromName(authorName);
+        AuthorValue postAuthor = postAuthor(authorEntity);
+        Long authorId = postAuthor.getId();
+        AuthorValue getAuthor = getAuthor(authorId);
+        assertThat(getAuthor.getName()).isEqualTo(authorName);
         String bookTitle = getRandomString();
-        Book book = new Book(bookTitle);
-        String jsonBook = MAPPER.writeValueAsString(book);
-        entity = new HttpEntity<>(jsonBook, headers);
-        restTemplate.exchange(String.format("%s/%s", AUTHOR_URL, id), HttpMethod.POST, entity, Void.class);
-
-        ResponseEntity<List<BookValue>> response = restTemplate.exchange(
-                String.format("%s/%s/books", AUTHOR_URL, id),
-                HttpMethod.GET,
-                new HttpEntity<>(headers),
-                new ParameterizedTypeReference<List<BookValue>>() {
-                });
-        List<BookValue> books = response.getBody();
+        HttpEntity<String> bookEntity = createBookEntityFromTitle(bookTitle);
+        postBook(bookEntity, authorId);
+        List<BookValue> books = getBooks(authorId);
         assertThat(books).hasSize(1);
-        assertThat(Objects.requireNonNull(books).get(0).getTitle()).isEqualTo(bookTitle);
-
-        restTemplate.exchange(String.format("%s/%s/%s", AUTHOR_URL, id, books.get(0).getId()),
-                HttpMethod.DELETE,
-                new HttpEntity<>(headers),
-                Void.class);
-
-        restTemplate.exchange(
-                String.format("%s/%s/books", AUTHOR_URL, id),
-                HttpMethod.GET,
-                new HttpEntity<>(headers),
-                new ParameterizedTypeReference<List<BookValue>>() {
-                });
-
-        restTemplate.exchange(
-                String.format("%s/%s", AUTHOR_URL, id),
-                HttpMethod.DELETE,
-                new HttpEntity<>(headers),
-                Void.class);
-
-        ResponseEntity<List<AuthorValue>> allAuthorsResponse = restTemplate.exchange(
-                String.format("%s", AUTHOR_URL),
-                HttpMethod.GET,
-                new HttpEntity<>(headers),
-                new ParameterizedTypeReference<List<AuthorValue>>() {
-                });
-        List<AuthorValue> authorValues = allAuthorsResponse.getBody();
+        assertThat(books.get(0).getTitle()).isEqualTo(bookTitle);
+        deleteBook(authorId, books.get(0).getId());
+        List<BookValue> bookValues = getBooks(authorId);
+        assertThat(bookValues).isEmpty();
+        deleteAuthor(authorId);
+        List<AuthorValue> authorValues = getAllAuthors();
         assertThat(authorValues).isEmpty();
-
-
     }
 
+    private HttpEntity<String> createAuthorEntityFromName(String authorName) throws JsonProcessingException {
+        AuthorValue authorValue = new AuthorValue(authorName);
+        String jsonAuthor = MAPPER.writeValueAsString(authorValue);
+        return new HttpEntity<>(jsonAuthor, basicHeaders());
+    }
+
+    private HttpEntity<String> createBookEntityFromTitle(String bookTitle) throws JsonProcessingException {
+        Book book = new Book(bookTitle);
+        String jsonBook = MAPPER.writeValueAsString(book);
+        return new HttpEntity<>(jsonBook, basicHeaders());
+    }
+
+    private List<AuthorValue> getAllAuthors() {
+        String format = String.format("%s", AUTHOR_URL);
+        return restTemplate.exchange(format, GET, justHeadersEntity(), listAuthorValue()).getBody();
+    }
+
+    private List<BookValue> getBooks(Long authorId) {
+        String url = String.format("%s/%s/book", AUTHOR_URL, authorId);
+        return restTemplate.exchange(url, GET, justHeadersEntity(), listBookValue()).getBody();
+    }
+
+    private void postBook(HttpEntity<String> entity, Long id) {
+        String format = String.format("%s/%s/book", AUTHOR_URL, id);
+        restTemplate.exchange(format, POST, entity, Void.class);
+    }
+
+    private AuthorValue getAuthor(Long authorId) {
+        String url = String.format("%s/%s", AUTHOR_URL, authorId);
+        return restTemplate.getForObject(url, AuthorValue.class);
+    }
+
+    private AuthorValue postAuthor(HttpEntity<String> entity) {
+        return restTemplate.exchange(AUTHOR_URL, POST, entity, AuthorValue.class).getBody();
+    }
+
+    private void deleteAuthor(Long id) {
+        String url = String.format("%s/%s", AUTHOR_URL, id);
+        restTemplate.exchange(url, DELETE, justHeadersEntity(), Void.class);
+    }
+
+    private void deleteBook(Long authorId, Long bookId) {
+        String format = String.format("%s/%s/%s", AUTHOR_URL, authorId, bookId);
+        restTemplate.exchange(format, DELETE, justHeadersEntity(), Void.class);
+    }
+
+    private HttpHeaders basicHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(APPLICATION_JSON));
+        headers.setContentType(APPLICATION_JSON);
+        return headers;
+    }
+
+    private HttpEntity<Object> justHeadersEntity() {
+        return new HttpEntity<>(basicHeaders());
+    }
+
+
+    private ParameterizedTypeReference<List<AuthorValue>> listAuthorValue() {
+        return new ParameterizedTypeReference<List<AuthorValue>>() {
+        };
+    }
+
+    private ParameterizedTypeReference<List<BookValue>> listBookValue() {
+        return new ParameterizedTypeReference<List<BookValue>>() {
+        };
+    }
 }
