7f30677cf096211aa991ea4244d412711fa85b05
yannisf <yannisf@ouranos>
2018-10-15 08:34:32 +0300

    Excluding certain findbugs findings

--- src/main/java/fraglab/library/AuthorRepository.java
+++ src/main/java/fraglab/library/AuthorRepository.java
@@ -1,7 +1,6 @@
 package fraglab.library;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
